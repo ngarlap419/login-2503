e2ffe51d7450f45754134c420717a95cf7d5e845
nithin437 <nithin.garlapati12@gmail.com>
2025-03-17 11:41:14 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Updating System"
+sudo apt update -y
+echo "Installing Utilities"
+sudo apt install -y zip unzip
+echo "Installing NGINX Web Server"
+sudo apt install -y nginx
+echo "Remove Sample Pages"
+sudo rm -rf /var/www/html
+echo "Clone Login App"
+sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
